73106726af01a54d5ec34ce7e4fbd9133b6dfe23
Kothuru <Ashwini.Kothuru@thomsonreuters.com>
2018-02-06 20:41:21 +0530

    Validated fields FName,Lname,Branch,Email

---
+++ src/app/Details.ts
@@ -0,0 +1,12 @@
+export class User {
+    FirstName: string;
+    LastName: string;
+    RollNumber : string;
+    Branch : string;
+    DOB: string;
+    YearofPassing : string;
+    Address : string;
+    CollegeName : string;
+    Email : string;
+    PhoneNumber : number
+} 
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { User } from '../Details';
 
 @Component({
   selector: 'app-form',
@@ -7,21 +8,53 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  form: FormGroup;
-    
-  firstName = new FormControl("", Validators.required);
-  constructor(fb: FormBuilder) { 
-    this.form = fb.group({
-      "firstName": this.firstName,
-      "password":["", Validators.required]
+  unamePattern = "^[a-zA-Z]{5,10}$";
+  lnamePattern = "^[a-zA-Z]{8,15}$";
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  BranchPattern="^[a-zA-z]{3}$";
+  RollPattern="^[0-9]{5}$";
+  isValidFormSubmitted = null;
+  
+  userForm = this.formBuilder.group({
+    FirstName: ['', [Validators.required,Validators.minLength(5), Validators.pattern(this.unamePattern)]],
+    LastName: ['', [Validators.required,Validators.minLength(5), Validators.pattern(this.lnamePattern)]],
+     Branch:['', [Validators.minLength(3), Validators.pattern(this.BranchPattern)]],
+    Email: ['', [Validators.required,Validators.pattern(this.emailPattern)]],
+    RollNumber: ['', [Validators.required,Validators.pattern(this.RollPattern)]],
   });
-  }
-  onSubmitModelBased() {
-    console.log("model-based form submitted");
-    console.log(this.form);
-}
-
+  
+  constructor(private formBuilder:FormBuilder) { }
+  
   ngOnInit() {
   }
-
+  
+  
+  
+  
+  onSubmit() {
+    this.isValidFormSubmitted = false;
+    if (this.userForm.invalid) {
+      return;
+    }
+    this.isValidFormSubmitted = true;
+    let user: User = this.userForm.value;
+    this.userForm.reset();
+  }
+  get FirstName() {
+    return this.userForm.get('FirstName');
+  }
+  
+  get LastName() {
+    return this.userForm.get('LastName');
+  }
+  get Branch() {
+    return this.userForm.get('Branch');
+  }
+  get RollNumber() {
+    return this.userForm.get('RollNumber');
+  }
+  
+  get Email() {
+    return this.userForm.get('Email');
+  }      
 }
