0e2e5a7f985c06445ea0725f353af99db725ac67
Kothuru <Ashwini.Kothuru@thomsonreuters.com>
2018-02-08 21:06:59 +0530

    Validated Phone,RollNo,Branch,Year,College

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,8 +12,12 @@ export class FormComponent implements OnInit {
   lnamePattern = "^[a-zA-Z]{8,15}$";
   emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
   BranchPattern="^[a-zA-z]{3}$";
-  RollPattern="^[0-9]{5}$";
+  RollPattern="^[0-9]{5}[a-zA-Z]{1}[0-9]{4}$";
   collegePattern = "^[a-zA-Z]$";
+  DOBPattern="^(1[0-2]|0?[1-9])/(3[01]|[12][0-9]|0?[1-9])/(?:[0-9]{2})?[0-9]{2}$";
+  YearPattern="^201[4-8]$";
+  PhonePattern= "^((\\+91-?)|0)?[0-9]{10}$";
+  AddressPattern="^[a-zA-Z0-9]{2}$";
   isValidFormSubmitted = null;
   
   userForm = this.formBuilder.group({
@@ -23,8 +27,21 @@ export class FormComponent implements OnInit {
     Email: ['', [Validators.required,Validators.pattern(this.emailPattern)]],
     College: ['', [Validators.required,Validators.pattern(this.collegePattern)]],
     RollNumber: ['', [Validators.required,Validators.pattern(this.RollPattern)]],
+    DOB: ['', [Validators.pattern(this.DOBPattern)]],
+    Year: ['', [Validators.pattern(this.YearPattern)]],
+    Phone: ['', [Validators.pattern(this.PhonePattern)]],
+    Address: ['', [Validators.pattern(this.AddressPattern)]],
   });
-  
+  UserDetails:Array<object>=[{FirstName: '',
+    LastName: '',
+    RollNumber : '',
+    Branch : '',
+    DOB: '',
+    Year : '',
+    Address : '',
+    College : '',
+    Email : '',
+    Phone : ''}];
   constructor(private formBuilder:FormBuilder) { }
   
   ngOnInit() {
@@ -35,6 +52,17 @@ export class FormComponent implements OnInit {
   
   onSubmit() {
     this.isValidFormSubmitted = false;
+    var data={FirstName: this.FirstName,
+      LastName: this.LastName,
+      RollNumber : this.RollNumber,
+      Branch : this.Branch,
+      DOB: this.DOB,
+      Year: this.Year,
+      Address : this.Address,
+      College : this.College,
+      Email : this.Email,
+      Phone : this.Phone}
+      this.UserDetails.push(data);
     if (this.userForm.invalid) {
       return;
     }
@@ -54,6 +82,23 @@ export class FormComponent implements OnInit {
   }
   get RollNumber() {
     return this.userForm.get('RollNumber');
+  }
+  get DOB()
+  {
+    return this.userForm.get('DOB');
+  }
+  get Year()
+  {
+    return this.userForm.get('Year');
+  }
+  get Address()
+  {
+    return this.userForm.get('Address');
+  }
+
+  get Phone()
+  {
+    return this.userForm.get('Phone');
   }
   get College() {
     return this.userForm.get('College');
